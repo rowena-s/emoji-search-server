698531f2b339bc8bb05e450ec8d53b5cd33daa2a
Rowena Stewart <rowena.stewart@industrieit.com>
2019-06-20 17:06:12 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+This server has been created by following
+
+https://programmingwithmosh.com/javascript/react-file-upload-proper-server-side-nodejs-easy/
+
---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var app = express();
+var multer = require('multer')
+var cors = require('cors');
+
+app.use(cors())
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+  cb(null, 'public')
+},
+filename: function (req, file, cb) {
+  cb(null, Date.now() + '-' +file.originalname )
+}
+})
+app.listen(8000, function() {
+
+  console.log('App running on port 8000');
+
+});
+
+var upload = multer({ storage: storage }).single('file')
+
+app.post('/upload',function(req, res) {
+   console.log("Called the upoad route");  
+  upload(req, res, function (err) {
+         if (err instanceof multer.MulterError) {
+             return res.status(500).json(err)
+         } else if (err) {
+             return res.status(500).json(err)
+         }
+    return res.status(200).send(req.file)
+
+  })
+
+});
